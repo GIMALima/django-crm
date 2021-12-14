f0246beaaf4f47fbff95b4f47e43d851ec967063
Alima Grine <alima.grine@lyra-network.com>
2021-12-14 14:18:06 +0100

    Added urls, models, and view setup.

---
+++ leads/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Lead
+
+def leads_list(request):
+    leads = Lead.objects.all()
+    context = {"leads" : leads}
+
+    return render(request, "leads/leads_list.html", context)
